bc1b2246a64b49ccb648c805af045b1072d1f853
14-Kanika <149942483+14-Kanika@users.noreply.github.com>
2024-01-29 13:53:14 +0000

    variables done

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 1444563
+let accountEmail = "abc@google.com"
+let accountPassword = "123456"
+accountCity = "Ludhiana"
+let accountName ;
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountName])
